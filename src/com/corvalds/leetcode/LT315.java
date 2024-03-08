735b4069fb2e0a939a375528a4ea32ae310663a6
chenzhenzhong <chenzhenzhong@cvte.com>
2024-03-08 18:02:51 +0800

    搭建LT-315题目类

---
+++ src/com/corvalds/leetcode/LT315.java
@@ -0,0 +1,9 @@
+package com.corvalds.leetcode;
+
+/**
+ * 问题简述：计算比自己小的数<br/>
+ * 问题思路：<br/>
+ * 问题链接：<a href="https://leetcode.com/problems/count-of-smaller-numbers-after-self/description">LT-315</a>
+ */
+public class LT315 {
+}
