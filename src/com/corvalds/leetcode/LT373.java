d1a8faf4853a4894ee54c94aa5d76afcc8f8544c
chenzhenzhong <chenzhenzhong@cvte.com>
2024-03-14 19:26:04 +0800

    搭建LT-373问题框架（没写出来）

---
+++ src/com/corvalds/leetcode/LT373.java
@@ -0,0 +1,14 @@
+package com.corvalds.leetcode;
+
+import java.util.List;
+
+/**
+ * 问题简述：寻找K个最小数值对<br/>
+ * 问题思路：<br/>
+ * 问题链接：<a href="https://leetcode.com/problems/find-k-pairs-with-smallest-sums/description/">LT-373</a>
+ */
+public class LT373 {
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+    }
+}
