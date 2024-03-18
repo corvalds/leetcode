c5b1c7e4546bfc72d7dfc6ad36617cdba1e74ac4
chenzhenzhong <chenzhenzhong@cvte.com>
2024-03-18 19:28:54 +0800

    完成LT-373，搭建LT-227框架（没写出来）

---
+++ src/com/corvalds/leetcode/LT227.java
@@ -0,0 +1,13 @@
+package com.corvalds.leetcode;
+
+/**
+ * 问题简述：<br/>
+ * 问题思路：<br/>
+ * 问题链接：<a href="https://leetcode.com/problems/basic-calculator-ii/description/">LT-227</a>
+ */
+public class LT227 {
+
+    public int calculate(String s) {
+        return -1;
+    }
+}
--- src/com/corvalds/leetcode/LT373.java
+++ src/com/corvalds/leetcode/LT373.java
@@ -1,15 +1,33 @@
 package com.corvalds.leetcode;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 问题简述：寻找K个最小数值对<br/>
- * 问题思路：<br/>
+ * 问题思路：将两个数组的元素元组构建成升序链表，然后按照合并k个升序链表的思路处理<br/>
  * 问题链接：<a href="https://leetcode.com/problems/find-k-pairs-with-smallest-sums/description/">LT-373</a>
  */
 public class LT373 {
 
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        return null;
+        List<List<Integer>> resultList = new LinkedList<>();
+
+        // 初始化优先队列
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparingInt((int[] a) -> (a[0] + a[1])));
+        for (int i = 0; i < nums1.length; i++) {
+            priorityQueue.add(new int[]{nums1[i], nums2[0], 0});
+        }
+
+        // 遍历找出当前最小的元组
+        while (k > 0) {
+            int[] currentMinTuple = priorityQueue.poll();
+            resultList.add(Arrays.asList(currentMinTuple[0], currentMinTuple[1]));
+            if (currentMinTuple[2] < nums2.length - 1) {
+                priorityQueue.add(new int[]{currentMinTuple[0], nums2[currentMinTuple[2] + 1], currentMinTuple[2] + 1});
+            }
+            k--;
+        }
+
+        return resultList;
     }
 }
