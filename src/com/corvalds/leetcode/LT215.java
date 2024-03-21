9a195e160476c85e124ad5093ccc627e9da6bc8b
chenzhenzhong <chenzhenzhong@cvte.com>
2024-03-21 19:12:38 +0800

    调整LT-215实现

--- src/com/corvalds/leetcode/LT215.java
+++ src/com/corvalds/leetcode/LT215.java
@@ -10,51 +10,49 @@ import java.util.Random;
 public class LT215 {
 
     public int findKthLargest(int[] nums, int k) {
-        // 需要先随机打乱数组确保快速选择算法的平均时间复杂度
+        // 使用快速选择算法进行第k大元素选择
+        // 1. 先对数组进行乱序排列
         shuffle(nums);
+        // 2. 借鉴快速排序思想，不断迭代尝试直到找到第k大的元素
+        k = nums.length - k;
         int p = -1;
         int lo = 0, hi = nums.length - 1;
-        while (p != (nums.length - k)) {
+        while (p != k) {
             p = partition(nums, lo, hi);
-            if (p < (nums.length - k)) {
-                // 当前已定位的数字排名比k小，需要到右区间去找更大的数字
-                lo = p + 1;
-            } else if (p > (nums.length - k)) {
-                // 当前已定位的数字排名比k大，需要到左区间去找更小的数字
+            if (p > k) {
                 hi = p - 1;
+            }
+            if (p < k) {
+                lo = p + 1;
             }
         }
 
-        return nums[nums.length - k];
+        return nums[k];
     }
 
     private void shuffle(int[] nums) {
         Random random = new Random();
         for (int i = 0; i < nums.length; i++) {
-            swap(nums, i, i + random.nextInt(nums.length - i));
+            swap(nums, i, random.nextInt(nums.length - i));
         }
     }
 
     private int partition(int[] nums, int lo, int hi) {
-        int p = lo;
-        // 使用两个指针相对而行，保证维护区间[p+1, i), (j, hi]的数字均小于nums[p]
+        int pivot = nums[lo];
+
         int i = lo + 1, j = hi;
         while (i <= j) {
-            while (i <= hi && nums[i] <= nums[p]) {
+            while (i <= hi && nums[i] <= pivot) {
                 i++;
             }
-
-            while (j >= lo + 1 && nums[j] > nums[p]) {
+            while (j >= lo && nums[j] > pivot) {
                 j--;
             }
-
-            if (i >= j) {
-                break;
+            if (i < j) {
+                swap(nums, i, j);
             }
-            swap(nums, i, j);
         }
-
-        swap(nums, p, j);
+        swap(nums, lo, j);
         return j;
     }
 
@@ -62,10 +60,5 @@ public class LT215 {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
-    }
-
-    public static void main(String[] args) {
-        // [2, 1] 1
-        System.out.println((new LT215()).findKthLargest(new int[]{2, 1}, 1));
     }
 }
